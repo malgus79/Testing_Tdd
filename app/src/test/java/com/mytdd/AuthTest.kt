25c5bd0255b1733cf8980d62dbe5f92311574eab
Gustavo Maldonado <gamcba7982@gmail.com>
2022-11-14 15:03:23 -0300

    Test login complete returns True/false

--- app/src/test/java/com/mytdd/AuthTest.kt
+++ app/src/test/java/com/mytdd/AuthTest.kt
@@ -1,4 +1,23 @@
 package com.mytdd
 
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
 class AuthTest {
+    @Test
+    fun login_complete_returnsTrue(){
+        val isAtuhenticated = userAuthentication("aaa@gmail.com", "1234")
+        assertTrue(isAtuhenticated)
+    }
+    @Test
+    fun login_complete_returnsFalse(){
+        val isAtuhenticated = userAuthentication("bb@gmail.com", "1234")
+        assertFalse(isAtuhenticated)
+    }
+    @Test
+    fun login_emptyEmail_returnsFalse(){
+        val isAtuhenticated = userAuthentication("", "1234")
+        assertFalse(isAtuhenticated)
+    }
 }
